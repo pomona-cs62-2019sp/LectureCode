f3b9ac2f110dfa272b07bdc6d7774f1cc0fdc5f7
Will Devanny <wdevanny@uci.edu>
2019-02-01 00:35:07 -0800

    Lecture 5 graphics code

---
+++ Lecture5/src/MyGUIDemo.java
@@ -0,0 +1,23 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MyGUIDemo extends JFrame{
+	public MyGUIDemo() {
+		super();
+		JPanel myPanel = new 
+				JPanel(new FlowLayout());
+			myPanel.add(new JLabel("Demo"));
+			myPanel.add(new JButton("Button"));
+		setContentPane(myPanel);
+    }
+	
+	public static void main(String[] args) {
+		MyGUIDemo demo = new MyGUIDemo();
+		demo.setSize(400, 400);
+		demo.setVisible(true);
+	}
+}
