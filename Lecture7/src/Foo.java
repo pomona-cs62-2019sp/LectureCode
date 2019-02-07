769f702badbccd525fa0d3f7cc32d17a7d4c6067
Will Devanny <wdevanny@uci.edu>
2019-02-06 22:43:28 -0800

    Generics and final lists

--- Lecture7/src/Foo.java
+++ Lecture7/src/Foo.java
@@ -1,6 +1,11 @@
 
 public class Foo {
 	public static void main(String[] args) {
-		IndexList myList = new ArrayIndexList();
+		final IndexList<Integer> myList = new ArrayIndexList<Integer>();
+		myList.add(0, 47);
+		IndexList<String> myList2 = new ArrayIndexList<String>();
+		myList2.add(0, "Data structures!");
+		IndexList<Double> myList3 = new ArrayIndexList<Double>();
+		myList3.add(0, 3.14);
 	}
 }
